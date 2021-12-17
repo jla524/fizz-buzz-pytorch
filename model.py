99146537260dad935c8badb3819072d2590a12ed
Jacky Lee <jla524@sfu.ca>
2021-12-17 01:39:57 -0800

    Add an extra layer to solve 1-100 (somewhat) consistently

--- model.py
+++ model.py
@@ -5,10 +5,11 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
+from tqdm import trange
 
 epochs = 10000
 bs = 128
-lr = 0.01
+lr = 0.001
 
 
 def encode_input(x):
@@ -31,25 +32,27 @@ def encode_fizz_buzz(x):
 class BuzzNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(10, 50)
-        self.l2 = nn.Linear(50, 4)
+        self.l1 = nn.Linear(10, 200)
+        self.l2 = nn.Linear(200, 200)
+        self.l3 = nn.Linear(200, 4)
 
     def forward(self, x):
         x = F.relu(self.l1(x))
-        x = self.l2(x)
+        x = F.relu(self.l2(x))
+        x = self.l3(x)
         return x
 
 
 def train(model, x_train, y_train):
     loss_func = nn.CrossEntropyLoss() 
-    optimizer = optim.Adam(net.parameters(), lr=lr)
-    for _ in range(epochs):
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    for _ in (t := trange(epochs)):
         samp = np.random.randint(0, len(x_train), size=(bs))
         x = torch.tensor(x_train[samp]).float()
         y = torch.tensor(y_train[samp])
         out = model(x)
         loss = loss_func(out, y)
-        print(f"loss {loss}")
+        t.set_description(f"loss {loss}")
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
@@ -62,25 +65,23 @@ def test(model, x_test, y_test):
     return accuracy
 
 
-def fizz_buzz(model, n):
+def fizz_buzz(n):
+    # Generate train and test data
+    x_train = np.array([encode_input(i) for i in range(101, 1024)])
+    y_train = np.array([encode_fizz_buzz(i) for i in range(101, 1024)])
+    x_test = np.array([encode_input(i) for i in range(1, 101)])
+    y_test = np.array([encode_fizz_buzz(i) for i in range(1, 101)])
+    # Define and train the neural network
+    net = BuzzNet()
+    train(net, x_train, y_train)
     answer = []
     for i in range(1, n + 1):
         options = [str(i), 'Fizz', 'Buzz', 'FizzBuzz']
         x = torch.tensor(encode_input(i)).reshape(1, 10).float()
-        cat = torch.argmax(model(x), dim=1)
+        cat = torch.argmax(net(x), dim=1)
         answer.append(options[cat])
     return answer
 
 
 if __name__ == '__main__':
-    # Train on 101-1023 and test on 1-100
-    x_train = np.array([encode_input(i) for i in range(101, 1024)])
-    y_train = np.array([encode_fizz_buzz(i) for i in range(101, 1024)])
-    x_test = np.array([encode_input(i) for i in range(1, 101)])
-    y_test = np.array([encode_fizz_buzz(i) for i in range(1, 101)])
-
-    # Train the neural network
-    net = BuzzNet()
-    train(net, x_train, y_train)
-    print(test(net, x_train, y_train))
-    print(fizz_buzz(net, 100))
+    print(fizz_buzz(100))
