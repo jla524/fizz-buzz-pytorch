2d9ffa12a68d511c02897326b41b854277c865b0
Jacky Lee <jla524@sfu.ca>
2021-12-17 19:04:25 -0800

    Save model when 100% accuracy is reached

--- model.py
+++ model.py
@@ -1,15 +1,17 @@
 """
 Solve fizz buzz using pytorch
 """
+from pathlib import Path
 import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
 from tqdm import trange
 
-epochs = 10000
+epochs = 7000
 bs = 128
-lr = 0.001
+lr = 0.003
+weights_path = Path('weights')
 
 
 def encode_input(x):
@@ -35,50 +37,66 @@ class BuzzNet(nn.Module):
         self.l1 = nn.Linear(10, 200)
         self.l2 = nn.Linear(200, 200)
         self.l3 = nn.Linear(200, 4)
+        self.dropout = nn.Dropout(p=0.2)
 
     def forward(self, x):
         x = F.relu(self.l1(x))
         x = F.relu(self.l2(x))
-        x = self.l3(x)
+        x = self.dropout(self.l3(x))
         return x
 
 
 def train(model, x_train, y_train):
-    loss_func = nn.CrossEntropyLoss() 
+    loss_func = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
+    model.train()
     for _ in (t := trange(epochs)):
         samp = np.random.randint(0, len(x_train), size=(bs))
         x = torch.tensor(x_train[samp]).float()
         y = torch.tensor(y_train[samp])
         out = model(x)
         loss = loss_func(out, y)
-        t.set_description(f"loss {loss}")
+        cat = torch.argmax(model(x), dim=1)
+        accuracy = (cat == y).float().mean()
+        t.set_description(f"loss = {loss} accuracy = {accuracy}")
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
 
 
 def test(model, x_test, y_test):
-    x = torch.tensor(x_train).float()
+    model.eval()
+    x = torch.tensor(x_test).float()
     cat = torch.argmax(model(x), dim=1).numpy()
     accuracy = (cat == y_test).mean()
+    print(f"accuracy = {accuracy}")
+    if accuracy == 1.0:
+        torch.save(model.state_dict(), weights_path)
     return accuracy
 
 
-def fizz_buzz(n):
-    # Generate train and test data
-    x_train = np.array([encode_input(i) for i in range(101, 1024)])
-    y_train = np.array([encode_fizz_buzz(i) for i in range(101, 1024)])
-    x_test = np.array([encode_input(i) for i in range(1, 101)])
-    y_test = np.array([encode_fizz_buzz(i) for i in range(1, 101)])
-    # Define and train the neural network
+def get_model():
     net = BuzzNet()
-    train(net, x_train, y_train)
+    if weights_path.is_file():
+        net.load_state_dict(torch.load(weights_path))
+        net.eval()
+    else:
+        x_train = np.array([encode_input(i) for i in range(101, 1024)])
+        y_train = np.array([encode_fizz_buzz(i) for i in range(101, 1024)])
+        x_test = np.array([encode_input(i) for i in range(1, 101)])
+        y_test = np.array([encode_fizz_buzz(i) for i in range(1, 101)])
+        train(net, x_train, y_train)
+        test(net, x_test, y_test)
+    return net
+
+
+def fizz_buzz(n):
     answer = []
+    model = get_model()
     for i in range(1, n + 1):
         options = [str(i), 'Fizz', 'Buzz', 'FizzBuzz']
         x = torch.tensor(encode_input(i)).reshape(1, 10).float()
-        cat = torch.argmax(net(x), dim=1)
+        cat = torch.argmax(model(x), dim=1)
         answer.append(options[cat])
     return answer
 
